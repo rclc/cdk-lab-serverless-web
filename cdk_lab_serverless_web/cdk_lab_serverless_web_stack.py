2d9499e7bdea18dea53a93dced3cfba292ad209b
EC2 Default User <ec2-user@ip-172-31-22-104.ec2.internal>
2024-01-22 04:37:50 +0000

    first commit - lambda function - happy.py - API Gateway Proxy

--- cdk_lab_serverless_web/cdk_lab_serverless_web_stack.py
+++ cdk_lab_serverless_web/cdk_lab_serverless_web_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_lambda as _lambda
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -11,7 +12,14 @@ class CdkLabServerlessWebStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler'
+            )
+        
         # example resource
         # queue = sqs.Queue(
         #     self, "CdkLabServerlessWebQueue",
